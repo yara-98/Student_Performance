e83313fcdfd4f9edfe856b5503a8c2a9832cf8f7
yara-98 <ykhatib98@gmail.com>
2024-02-17 10:51:21 +0300

    Finished the comparison in the parental table

--- student-performance-dashboard/src/components/parentTable.vue
+++ student-performance-dashboard/src/components/parentTable.vue
@@ -1,40 +1,117 @@
+import './assets/style.css';
 <template>
   <div>
-    <table class="w-16 md:w-32 lg:w-48">
-      <thead>
+    <sorted-table class="w-full">
+      <thead class="border-collapse border border-slate-500">
         <tr>
-          <th>Gender</th>
-          <th>Average Score</th>
+          <th
+            scope="col"
+            class="border border-gray-200 py-2 px-4 text-center w-1/3"
+          >
+            <sort-link name="id">Job</sort-link>
+          </th>
+          <th
+            scope="col"
+            class="border border-gray-200 py-2 px-4 text-center w-1/3"
+          >
+            <sort-link name="name">Female</sort-link>
+          </th>
+          <th
+            scope="col"
+            class="border border-gray-200 py-2 px-4 text-center w-1/3"
+          >
+            <sort-link name="name">Male</sort-link>
+          </th>
+          <th
+            scope="col"
+            class="border border-gray-200 py-2 px-8 text-center w-1/3"
+          >
+            <sort-link name="name">Total</sort-link>
+          </th>
         </tr>
       </thead>
-      <tbody>
-        <tr v-for="row in tableData" :key="row.sex"></tr>
+      <tbody class="bg-gray-100">
+        <template v-for="(item, index) in data" :key="index">
+          <tr class="border-b text-center border border-slate-700">
+            <td>{{ item.parent_job }}</td>
+            <td>{{ item.count }}</td>
+            <!-- Third row for dataM -->
+            <td>{{ dataM[index] ? dataM[index].total_count_m : '-' }}</td>
+            <td>
+              {{ item.count + (dataM[index] ? dataM[index].total_count_m : 0) }}
+            </td>
+          </tr>
+        </template>
       </tbody>
-    </table>
+    </sorted-table>
   </div>
 </template>
 <script>
-import { ref } from 'vue';
+// import axios from 'axios';
+// export default {
+//   data() {
+//     return {
+//       loading: true, // Assuming you have a loading indicator
+//       data: [], // Initialize data property
+//     };
+//   },
+//   mounted() {
+//     axios
+//       .get('http://localhost:4000/parent')
+//       .then((response) => {
+//         this.data = response.data; // Assigning data to the data property
+//         this.loading = false; // Set loading to false once data is received
+//       })
+//       .catch((error) => {
+//         console.error('Error fetching data:', error);
+//         this.loading = false; // Set loading to false on error
+//       });
+//   },
+// };
 import axios from 'axios';
 export default {
-  setup() {
-    const result = ref(null);
+  data() {
+    return {
+      loading: true,
+      data: [],
+      dataM: [],
+    };
+  },
+  mounted() {
     axios
-      .get('http://localhost:4000/averageScores')
-      .then((data) => (result.value = data));
-    return { result };
+      .get('http://localhost:4000/parent')
+      .then((response) => {
+        this.data = response.data;
+      })
+      .catch((error) => {
+        console.error('Error fetching parentWork data:', error);
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+
+    axios
+      .get('http://localhost:4000/parentM')
+      .then((response) => {
+        this.dataM = response.data;
+      })
+      .catch((error) => {
+        console.error('Error fetching parentM data:', error);
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    responsive: true,
+    maintainAspectRatio: false, // false to make the chart filling the space
+    aspectRatio: 1.5,
   },
 };
 </script>
-<style>
-th,
-td {
-  border: 1px solid #ddd;
-  padding: 3px;
-  text-align: center;
-}
-
-th {
-  background-color: #f2f2f2;
-}
-</style>
+<style></style>
