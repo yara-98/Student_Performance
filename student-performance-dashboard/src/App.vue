dbf6d4889a5ef14dac693d2112e78a89c9f44982
yara-98 <ykhatib98@gmail.com>
2024-02-17 10:45:29 +0300

    delete commented code

--- student-performance-dashboard/src/App.vue
+++ student-performance-dashboard/src/App.vue
@@ -12,16 +12,6 @@ export default {
   <main>
     <performanceDashboard />
   </main>
-  <!-- <div class="flex flex-col lg:flex-row"> -->
-  <!-- this section will be the left section -->
-  <!-- <div class="w-1/2 bg-gray-400">
-      <barChart />
-    </div> -->
-  <!-- this section will be the right section -->
-  <!-- <div class="w-1/2 bg-gray-400">
-      <barChart />
-    </div>
-  </div> -->
 </template>
 
 <style></style>
